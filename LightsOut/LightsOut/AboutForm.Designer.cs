d3d2947815d979cd122e47c53bd6ec9ae473f934
Casey Griggs <kcgymgrl@gmail.com>
2019-08-28 19:34:26 -0500

    Add LightsOut project files

---
+++ LightsOut/LightsOut/AboutForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace LightsOut
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GameName = new System.Windows.Forms.Label();
+            this.AuthorName = new System.Windows.Forms.Label();
+            this.Instructions = new System.Windows.Forms.Label();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.LightBulbImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LightBulbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GameName
+            // 
+            this.GameName.AutoSize = true;
+            this.GameName.Font = new System.Drawing.Font("Year supply of fairy cakes", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GameName.ForeColor = System.Drawing.Color.Teal;
+            this.GameName.Location = new System.Drawing.Point(115, 18);
+            this.GameName.Name = "GameName";
+            this.GameName.Size = new System.Drawing.Size(140, 32);
+            this.GameName.TabIndex = 0;
+            this.GameName.Text = "Lights Out!";
+            // 
+            // AuthorName
+            // 
+            this.AuthorName.AutoSize = true;
+            this.AuthorName.Font = new System.Drawing.Font("X-Files", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AuthorName.Location = new System.Drawing.Point(119, 50);
+            this.AuthorName.Name = "AuthorName";
+            this.AuthorName.Size = new System.Drawing.Size(102, 12);
+            this.AuthorName.TabIndex = 2;
+            this.AuthorName.Text = "By Casey Griggs";
+            // 
+            // Instructions
+            // 
+            this.Instructions.AutoSize = true;
+            this.Instructions.Font = new System.Drawing.Font("X-Files", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Instructions.Location = new System.Drawing.Point(33, 89);
+            this.Instructions.Name = "Instructions";
+            this.Instructions.Size = new System.Drawing.Size(251, 12);
+            this.Instructions.TabIndex = 3;
+            this.Instructions.Text = "Turn all the lights out to win the game!";
+            this.Instructions.Click += new System.EventHandler(this.Instructions_Click);
+            // 
+            // OKButton
+            // 
+            this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OKButton.Location = new System.Drawing.Point(117, 114);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 4;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // LightBulbImage
+            // 
+            this.LightBulbImage.Image = global::LightsOut.Properties.Resources.LightBulb3;
+            this.LightBulbImage.Location = new System.Drawing.Point(53, 18);
+            this.LightBulbImage.Name = "LightBulbImage";
+            this.LightBulbImage.Size = new System.Drawing.Size(56, 56);
+            this.LightBulbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LightBulbImage.TabIndex = 1;
+            this.LightBulbImage.TabStop = false;
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 161);
+            this.Controls.Add(this.OKButton);
+            this.Controls.Add(this.Instructions);
+            this.Controls.Add(this.AuthorName);
+            this.Controls.Add(this.LightBulbImage);
+            this.Controls.Add(this.GameName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            ((System.ComponentModel.ISupportInitialize)(this.LightBulbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label GameName;
+        private System.Windows.Forms.PictureBox LightBulbImage;
+        private System.Windows.Forms.Label AuthorName;
+        private System.Windows.Forms.Label Instructions;
+        private System.Windows.Forms.Button OKButton;
+    }
+}
---
+++ LightsOut/LightsOut/MainForm.cs
@@ -0,0 +1,214 @@
+﻿// Casey Griggs
+// COMP 4450
+// McCown
+// Lights Out
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut
+{
+    public partial class MainForm : Form
+    {
+        private const int GridOffset = 25;
+        private const int GridLength = 200;
+        private int NumCells = 3;
+        private int CellLength;
+
+        private bool[,] grid;
+        private Random rand;
+        public MainForm()
+        {
+            InitializeComponent();
+            this.x3ToolStripMenuItem.Checked = true;
+            this.x3ToolStripMenuItem.CheckState = CheckState.Checked;
+            CellLength = GridLength / NumCells;
+            rand = new Random();
+            grid = new bool[NumCells, NumCells];
+
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = true;
+        }
+
+        private void HelpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int r = 0; r < NumCells; r++)
+            {
+                for (int c = 0; c < NumCells; c++)
+                {
+                    Brush brush;
+                    Pen pen;
+
+                    if (grid[r, c])
+                    {
+                        pen = Pens.Black;
+                        brush = Brushes.White;
+                    }
+                    else
+                    {
+                        pen = Pens.White;
+                        brush = Brushes.Black;
+                    }
+
+                    int x = c * CellLength + GridOffset;
+                    int y = r * CellLength + GridOffset;
+
+                    g.DrawRectangle(pen, x, y, CellLength, CellLength);
+                    g.FillRectangle(brush, x + 1, y + 1, CellLength - 1, CellLength - 1);
+                }
+            }
+        }
+
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.X < GridOffset || e.X > CellLength * NumCells + GridOffset ||
+                e.Y < GridOffset || e.Y > CellLength * NumCells + GridOffset)
+                return;
+
+            int r = (e.Y - GridOffset) / CellLength;
+            int c = (e.X - GridOffset) / CellLength;
+
+            for (int i = r - 1; i <= r + 1; i++)
+                for (int j = c - 1; j <= c + 1; j++)
+                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
+                        grid[i, j] = !grid[i, j];
+
+            this.Invalidate();
+
+            if (PlayerWon())
+            {
+                MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private bool PlayerWon()
+        {     
+            for (int r = 0; r < NumCells; r++)
+            {
+                for (int c = 0; c < NumCells; c++)
+                {
+                    if (grid[r, c])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            for (int r = 0; r < NumCells; r++)
+                for (int c = 0; c < NumCells; c++)
+                    grid[r, c] = rand.Next(2) == 1;
+
+            this.Invalidate();
+        }
+
+        private void NewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            NewGameButton_Click(sender, e);
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutForm aboutBox = new AboutForm();
+            aboutBox.ShowDialog(this);
+        }
+
+        private void X3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem temp = this.x3ToolStripMenuItem;
+            if (temp.Checked == false)
+            {
+                temp.Checked = true;
+                temp.CheckState = CheckState.Checked;
+                NumCells = 3;
+                CellLength = GridLength / NumCells;
+                rand = new Random();
+                grid = new bool[NumCells, NumCells];
+
+                for (int r = 0; r < NumCells; r++)
+                    for (int c = 0; c < NumCells; c++)
+                        grid[r, c] = true;
+                this.x4ToolStripMenuItem.Checked = false;
+                this.x4ToolStripMenuItem.CheckState = CheckState.Unchecked;
+                this.x5ToolStripMenuItem.Checked = false;
+                this.x5ToolStripMenuItem.CheckState = CheckState.Unchecked;
+                Invalidate();
+            }           
+        }
+
+        private void X4ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem temp = this.x4ToolStripMenuItem;
+            if (temp.Checked == false)
+            {
+                temp.Checked = true;
+                temp.CheckState = CheckState.Checked;
+                NumCells = 4;
+                CellLength = GridLength / NumCells;
+                rand = new Random();
+                grid = new bool[NumCells, NumCells];
+
+                for (int r = 0; r < NumCells; r++)
+                    for (int c = 0; c < NumCells; c++)
+                        grid[r, c] = true;
+                this.x3ToolStripMenuItem.Checked = false;
+                this.x3ToolStripMenuItem.CheckState = CheckState.Unchecked;
+                this.x5ToolStripMenuItem.Checked = false;
+                this.x5ToolStripMenuItem.CheckState = CheckState.Unchecked;
+                Invalidate();
+            }
+        }
+
+        private void X5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem temp = this.x5ToolStripMenuItem;
+            if (temp.Checked == false)
+            {
+                temp.Checked = true;
+                temp.CheckState = CheckState.Checked;
+                NumCells = 5;
+                CellLength = GridLength / NumCells;
+                rand = new Random();
+                grid = new bool[NumCells, NumCells];
+
+                for (int r = 0; r < NumCells; r++)
+                    for (int c = 0; c < NumCells; c++)
+                        grid[r, c] = true;
+                this.x3ToolStripMenuItem.Checked = false;
+                this.x3ToolStripMenuItem.CheckState = CheckState.Unchecked;
+                this.x4ToolStripMenuItem.Checked = false;
+                this.x4ToolStripMenuItem.CheckState = CheckState.Unchecked;
+                Invalidate();
+            }
+        }
+    }
+}
