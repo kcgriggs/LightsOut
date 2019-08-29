504a6fee4b6ca988b9a4707681640660008b6803
Casey Griggs <kcgymgrl@gmail.com>
2019-08-29 13:11:54 -0500

    Add and integrate LightsOutGame class

--- LightsOut/LightsOut/MainForm.cs
+++ LightsOut/LightsOut/MainForm.cs
@@ -1,7 +1,7 @@
 ﻿// Casey Griggs
 // COMP 4450
 // McCown
-// Lights Out
+// Lights Out - Git
 
 using System;
 using System.Collections.Generic;
@@ -19,23 +19,16 @@ namespace LightsOut
     {
         private const int GridOffset = 25;
         private const int GridLength = 200;
-        private int NumCells = 3;
         private int CellLength;
-
-        private bool[,] grid;
+        private LightsOutGame game;
+ 
         private Random rand;
         public MainForm()
         {
             InitializeComponent();
+            game = new LightsOutGame();
             this.x3ToolStripMenuItem.Checked = true;
-            this.x3ToolStripMenuItem.CheckState = CheckState.Checked;
-            CellLength = GridLength / NumCells;
-            rand = new Random();
-            grid = new bool[NumCells, NumCells];
-
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = true;
+            CellLength = GridLength / game.GridSize;
         }
 
         private void HelpToolStripMenuItem_Click(object sender, EventArgs e)
@@ -47,14 +40,14 @@ namespace LightsOut
         {
             Graphics g = e.Graphics;
 
-            for (int r = 0; r < NumCells; r++)
+            for (int r = 0; r < game.GridSize; r++)
             {
-                for (int c = 0; c < NumCells; c++)
+                for (int c = 0; c < game.GridSize; c++)
                 {
                     Brush brush;
                     Pen pen;
 
-                    if (grid[r, c])
+                    if (game.GetGridValue(r, c))
                     {
                         pen = Pens.Black;
                         brush = Brushes.White;
@@ -76,47 +69,28 @@ namespace LightsOut
 
         private void MainForm_MouseDown(object sender, MouseEventArgs e)
         {
-            if (e.X < GridOffset || e.X > CellLength * NumCells + GridOffset ||
-                e.Y < GridOffset || e.Y > CellLength * NumCells + GridOffset)
+            if (e.X < GridOffset || e.X > CellLength * game.GridSize + GridOffset ||
+                e.Y < GridOffset || e.Y > CellLength * game.GridSize + GridOffset)
                 return;
 
             int r = (e.Y - GridOffset) / CellLength;
             int c = (e.X - GridOffset) / CellLength;
 
-            for (int i = r - 1; i <= r + 1; i++)
-                for (int j = c - 1; j <= c + 1; j++)
-                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
-                        grid[i, j] = !grid[i, j];
+            game.Move(r, c);
 
             this.Invalidate();
 
-            if (PlayerWon())
+            if (game.IsGameOver())
             {
                 MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
                                 MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
-        private bool PlayerWon()
-        {     
-            for (int r = 0; r < NumCells; r++)
-            {
-                for (int c = 0; c < NumCells; c++)
-                {
-                    if (grid[r, c])
-                    {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
 
         private void NewGameButton_Click(object sender, EventArgs e)
         {
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = rand.Next(2) == 1;
+            game.NewGame();
 
             this.Invalidate();
         }
@@ -144,69 +118,45 @@ namespace LightsOut
 
         private void X3ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ToolStripMenuItem temp = this.x3ToolStripMenuItem;
-            if (temp.Checked == false)
+            if (this.x3ToolStripMenuItem.Checked == false)
             {
-                temp.Checked = true;
-                temp.CheckState = CheckState.Checked;
-                NumCells = 3;
-                CellLength = GridLength / NumCells;
-                rand = new Random();
-                grid = new bool[NumCells, NumCells];
+                this.x3ToolStripMenuItem.Checked = true;
 
-                for (int r = 0; r < NumCells; r++)
-                    for (int c = 0; c < NumCells; c++)
-                        grid[r, c] = true;
+                game.GridSize = 3;
+                CellLength = GridLength / game.GridSize;
+                
                 this.x4ToolStripMenuItem.Checked = false;
-                this.x4ToolStripMenuItem.CheckState = CheckState.Unchecked;
                 this.x5ToolStripMenuItem.Checked = false;
-                this.x5ToolStripMenuItem.CheckState = CheckState.Unchecked;
                 Invalidate();
             }           
         }
 
         private void X4ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ToolStripMenuItem temp = this.x4ToolStripMenuItem;
-            if (temp.Checked == false)
+            if (this.x4ToolStripMenuItem.Checked == false)
             {
-                temp.Checked = true;
-                temp.CheckState = CheckState.Checked;
-                NumCells = 4;
-                CellLength = GridLength / NumCells;
-                rand = new Random();
-                grid = new bool[NumCells, NumCells];
+                this.x4ToolStripMenuItem.Checked = true;
 
-                for (int r = 0; r < NumCells; r++)
-                    for (int c = 0; c < NumCells; c++)
-                        grid[r, c] = true;
+                game.GridSize = 4;
+                CellLength = GridLength / game.GridSize;
+
                 this.x3ToolStripMenuItem.Checked = false;
-                this.x3ToolStripMenuItem.CheckState = CheckState.Unchecked;
                 this.x5ToolStripMenuItem.Checked = false;
-                this.x5ToolStripMenuItem.CheckState = CheckState.Unchecked;
                 Invalidate();
             }
         }
 
         private void X5ToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            ToolStripMenuItem temp = this.x5ToolStripMenuItem;
-            if (temp.Checked == false)
+            if (this.x5ToolStripMenuItem.Checked == false)
             {
-                temp.Checked = true;
-                temp.CheckState = CheckState.Checked;
-                NumCells = 5;
-                CellLength = GridLength / NumCells;
-                rand = new Random();
-                grid = new bool[NumCells, NumCells];
+                this.x5ToolStripMenuItem.Checked = true;
 
-                for (int r = 0; r < NumCells; r++)
-                    for (int c = 0; c < NumCells; c++)
-                        grid[r, c] = true;
+                game.GridSize = 5;
+                CellLength = GridLength / game.GridSize;
+
                 this.x3ToolStripMenuItem.Checked = false;
-                this.x3ToolStripMenuItem.CheckState = CheckState.Unchecked;
                 this.x4ToolStripMenuItem.Checked = false;
-                this.x4ToolStripMenuItem.CheckState = CheckState.Unchecked;
                 Invalidate();
             }
         }
